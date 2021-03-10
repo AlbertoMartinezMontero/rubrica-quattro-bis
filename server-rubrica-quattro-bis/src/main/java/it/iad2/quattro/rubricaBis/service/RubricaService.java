917455cd5099e29e0b97314001a8a8d3774f9bc6
danieleSantini <danielesantini94@gmail.com>
2021-03-10 19:09:56 +0100

    commit #1
    completamento lato server

---
+++ server-rubrica-quattro-bis/src/main/java/it/iad2/quattro/rubricaBis/service/RubricaService.java
@@ -0,0 +1,15 @@
+package it.iad2.quattro.rubricaBis.service;
+
+import it.iad2.quattro.rubricaBis.dto.ListaContattiDto;
+import it.iad2.quattro.rubricaBis.model.Contatto;
+
+public interface RubricaService {
+    
+    ListaContattiDto aggiungi(Contatto contatto);
+
+    ListaContattiDto elimina(Contatto contatto);
+
+    ListaContattiDto ricerca(String parametro);
+
+    ListaContattiDto aggiorna();
+}
---
+++ server-rubrica-quattro-bis/src/main/java/it/iad2/quattro/rubricaBis/serviceimpl/RubricaServiceImpl.java
@@ -0,0 +1,50 @@
+package it.iad2.quattro.rubricaBis.serviceimpl;
+
+import it.iad2.quattro.rubricaBis.dto.ListaContattiDto;
+import it.iad2.quattro.rubricaBis.model.Contatto;
+import it.iad2.quattro.rubricaBis.repository.RubricaRepository;
+import it.iad2.quattro.rubricaBis.service.RubricaService;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RubricaServiceImpl implements RubricaService {
+    
+    @Autowired
+    RubricaRepository rbrepository;
+
+    @Override
+    public ListaContattiDto aggiungi(Contatto contatto) {
+        rbrepository.save(contatto);
+        return aggiorna();
+    }
+
+    @Override
+    public ListaContattiDto elimina(Contatto contatto) {
+        rbrepository.delete(contatto);
+        return aggiorna();
+
+    }
+
+    @Override
+    public ListaContattiDto ricerca(String parametro) {
+        List<Contatto> trovati = rbrepository.findByNome(parametro);
+        return new ListaContattiDto(trovati);
+    }
+
+    @Override
+    public ListaContattiDto aggiorna() {
+        
+        ListaContattiDto dto = new ListaContattiDto();
+        List<Contatto> listaContatti = rbrepository.findAll();
+        if (listaContatti == null) {
+            dto.setListaContatti(new ArrayList<>());
+        } else {
+            dto.setListaContatti(listaContatti);
+        }
+        return dto;
+    }
+
+}
