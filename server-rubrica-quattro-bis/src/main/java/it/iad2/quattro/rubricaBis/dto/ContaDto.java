995338a9d2ef6b3faad1dd1a31e83f5f78b052ad
Valerio1990 <valeriopiccioni1990@gmail.com>
2021-03-11 01:15:20 +0100

    Commit 6
    Creazione lato server metodo conta

---
+++ server-rubrica-quattro-bis/src/main/java/it/iad2/quattro/rubricaBis/dto/ContaDto.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.iad2.quattro.rubricaBis.dto;
+
+/**
+ *
+ * @author Valerio
+ */
+public class ContaDto {
+    
+    private long conteggio;
+
+    public ContaDto() {
+    }
+
+    public ContaDto(long conteggio) {
+        this.conteggio = conteggio;
+    }
+
+    public long getConteggio() {
+        return conteggio;
+    }
+
+    public void setConteggio(long conteggio) {
+        this.conteggio = conteggio;
+    }
+    
+    
+}
